e71eca7eccd6b3a8b989cba5453f3e721beb6ed8
Lyn <jen.l@mailfence.com>
2018-09-07 00:05:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout (string) {
+return string.toUpperCase(); // HELLO! 
+}
+
+function whisper (string) {
+return string.toLowerCase();
+}
+
+
+function logShout (string) {
+  console.log (string.toUpperCase());
+}
+
+logShout();
+
+function logWhisper (string) {
+  console.log (string.toLowerCase());
+}
+logShout();
+
+function sayHiToGrandma (string) {
+  if (string.toLowerCase()) {
+  return 'I can\'t hear you!'}
+}
+
+sayHiToGrandma()
+
+function sayHiToGrandma (string) {
+  if (string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
+sayHiToGrandma()
+
+function sayHiToGrandma (string) {
+  if (string === "I love you, Grandma") {
+    return "I love you, too."}
+}
+sayHiToGrandma()
