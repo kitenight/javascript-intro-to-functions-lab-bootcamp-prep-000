7049d3197cb3fdaf1040f1bb35ceca4015c94b5f
Lyn <jen.l@mailfence.com>
2018-09-07 00:09:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,18 +20,16 @@ logShout();
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase()) {
-  return 'I can\'t hear you!'}
-}
-
-
-
-function sayHiToGrandma (string) {
-  if (string.toUpperCase()) {
+  return 'I can\'t hear you!'
+    
+  }
+    if (string.toUpperCase()) {
     return "YES INDEED!"
   }
-}
-
-function sayHiToGrandma (string) {
+  
   if (string === "I love you, Grandma") {
-    return "I love you, too."}
+    return "I love you, too."
+    
+  }
+  
 }
